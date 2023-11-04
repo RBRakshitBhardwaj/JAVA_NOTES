9d86a0f38a038708a36801b14a757d20cddc7fb0
RBRakshitBhardwaj <rb.rakshit.bhardwaj@gmail.com>
2023-11-05 04:16:37 +0530

    ADD 7 MORE ABOUT DATA TYPE TYPES ON BASES OF CATEGORY AND PLACE

---
+++ JAVA NOTES/7_MORE INFO ABOUT D-TYPE/D_type_Class_Range.java
@@ -0,0 +1,13 @@
+public class D_type_Class_Range {
+    public static void main(String[] args) {
+        System.out.println("Byte min value is "+ Byte.MIN_VALUE);
+		System.out.println("Byte max value is "+ Byte.MAX_VALUE);
+		
+		System.out.println("Integer Min value is "+ Integer.MIN_VALUE);
+		System.out.println("Integer max value is "+ Integer.MAX_VALUE);
+
+        System.out.println("Char min value is "+ (int)Character.MIN_VALUE);
+        System.out.println("Char max value "+ (int)Character.MAX_VALUE);
+    }
+    
+}
---
+++ JAVA NOTES/7_MORE INFO ABOUT D-TYPE/MoreD_Type.java
@@ -0,0 +1,91 @@
+public class MoreD_Type {
+    public static void main(String[] args) {
+
+        // prove  JAVA IS NOT CASE SENSETIVE 
+        System.out.println("it all about implementation");
+		char ch='\u004E';
+		char che='\u004e';
+		
+		
+		System.out.println(ch);
+		System.out.println(che);
+
+
+/*
+ * 
+ * char take 2 byte in java 
+ * 
+ * \u0000 this is lower value in char
+ * \uFFFF is the highest va;uje of unicode
+ * 
+ *  java take 2 byte for char because  of unicode  2 BYTE = 16 BITS.
+ * 
+ * and c take 1 byte for char because of byte code   I BYTE = 8 BITS
+ * 
+ * unicode   UNIVERSAL CODE
+ * -->  65536
+ * 
+ * AASCI  AMERICAN STANDARD CODE FOR INFORMATION INTERCHANGE
+ * --> 256
+ * 
+ * 
+ * 
+ * SOME TIME JAVA IS NOT CASE SENSETIVE
+ * '\u004E' and '\u004e' both are same
+ * 
+ * 
+ * 
+ * java 8 11 17    famous
+ * 
+ * 
+ * THERE ARE CALSS FOR EVERY DATATYPE
+ * byte    -->   Byte 
+ * short   -->   Short
+ * long     -->   Long
+ * int     --> Integer   
+ * char    -->  Character
+ * 
+ * if is a class the first letter is capital
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
