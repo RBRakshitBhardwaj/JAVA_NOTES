ef62043879e32c9d2fe3ae8977d3349a0faa6e6d
RBRakshitBhardwaj <rb.rakshit.bhardwaj@gmail.com>
2023-11-11 23:12:56 +0530

    data which has not been upload

---
+++ JAVA NOTES/10_/Demo.java
@@ -0,0 +1,64 @@
+public class Demo {
+    public static void main(String[] args) {
+        
+
+
+
+
+        /*
+         * 
+         * 
+         *  java use uni-code it is not mean that it is not using ascii 
+         * ASCII ranges form 0 - 255
+         * 
+         * but 
+         * Unicode : 0 - 65535
+         * 
+         * A-Z -> 65 - 90
+         * 
+         * a-z -> 48 - 57
+         * 
+         * 0-9 -> 48 - 57
+         * space bar --> 32
+         *  
+         * ascii keyboard set reserved from 0 - 499 chars.
+         * 
+         * 
+         * >>>>>>>>>>> Memory Management By JVM <<<<<<<<<<<<<<<<<<<<<
+         * 
+         * JVM will split the memortry  into different parts as below :
+         * 
+         *      High Order Memory
+         *        Shared Memory
+         *         Heap Memory
+         *        Static Memory
+         *        Global Memory 
+         * 
+         * --> a normal student know :
+         *  our program always get executed in the stack memory.
+         * 
+         * 
+         * >>>>>>>>>>>>>>High Order Memory <<<<<<<<<<<<
+         * 
+         * To store all the command line parameter
+         * 
+         * When we start giving the value to main  [psv main (Stirng args[])]   to store all command line Argument or parameters 
+         *  os environment -> path class path.
+         * 
+         * >>>>>>>>>>>>>>>>>>>>>> Stack Memory <<<<<<<<<<<<<<<<
+         * 
+         * All methods always get executed in the stack memory.
+         * 
+         * To Store all local variable / method variable.
+         * Memory allocation is LIFO (Last In First Out) based.
+         * This memory space grows automatically as long as sharable memory available for it.
+         * 
+         * 
+         * 18:14 16-8-23
+         * 
+         * 
+         * 
+         */
+    }
+    
+}
--- JAVA NOTES/9_/Ddemo.java
+++ JAVA NOTES/9_/Ddemo.java
@@ -14,21 +14,60 @@ public class Ddemo {
  * 
  * for name 
  * String name=rakshtit;
- * 
+ *                      --> Statically typed
  * int age =20;
  * 
  * but now
  * >>>>>>>>>>>  LOcal variablbe Type Inference <<<<<<<<<
  * var name " rakshit "
- * var age=20; 
+ * var age=20;           --> Dynamically typed
  * 
  * 
  * 
+ * var cannot be used in an instance and global/static class variable declaration .
+ * 
+ * that why its name is  local type inference.
+ *   
+ * we cannot use var here (instance variable): calss variable declaration
+ * 
+ * class Demo:{
+ *      var x=50; //instance variable
+ *      public static void main(Stirng[]arg){
+ *      System.out.println(x)
+ *      
+ *      }
+ * 
+ * }
  * 
  * 
  * 
+ * >>>>>>>>>>>>>>Type inference of var <<<<<<<<<<<<<<<<<<
+ * 
+ * public void reassignment(){
+ *    var number =7; //okk
+ *    number =4; // okk
+ *    number ="five"; // dosent compile   -->it is string
+ *          we cannot change the type once we assign the variable .
+ *    
+ * 
+ *      }
+ * 
+ * Note:
+ *      each and every data goes in memory in the 0 & 1 form
+ * 
+ *    
+ * Binary:
+ *  2 4 8 16 32 64 128
+ * 
+ *  71 = 64+4+2+1
+ *       
+ * 64 32 16 8 4 2 1
+ *  1  0 0  0 1 1 1 = 71 
+ * 
+ * \t is use for giving a tab space to program
  * 
  * 
+ * 16 -8 -23
  * 
  * 
  * 
