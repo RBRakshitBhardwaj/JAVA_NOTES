dd9bc9f544d2d249a5eadfcb9fc2940a5e5063a1
RBRakhitBhardwaj <rb.rakshit.bhardwaj@gmail.com>
2023-12-30 04:09:09 +0530

    more on  operator

--- Relational operator/relation.java
+++ Relational operator/relation.java
@@ -83,15 +83,70 @@ public class relation {
          *  0    0   0    0   0  1  0  1  0   = 10
          *  0    0   0    0   0  0  0  1  0 right shift by 2 = 2
          * 
+         * Trick --> 
+         * ex
+         *      13>>2  =  13/2^3 = 1
+         *      13<<2  =  13*2^3 = 104
+         * 
+         * in case of left shift operator we do the multiplicatopn 
+         * where as in case of right shift case operator we do the devidion
+         * 
+         * int a =10;
+         * int b=10;
          * 
          * 
+         *  if(a++ == 10 | b++ == 10){          //  all task will perform
+         *          System.out.pritnln(a+" "+b);    // 11    11
+         *  }
          * 
-         * 
+         * int c=10;
+         * int d=10;
+         * if(c++ == 10||d++ == 10){  // here short sircuit will perform                                    
+         *      System.out.println(c+" "+d);   //     11   10
+         *  }
          *  
          * 
+         * NOte    Logical operator always take short-cut short circuit    && || !
          * 
          * 
+         * if(duck!=null & duck.getAge()<5)
          * 
+         * it will rais exception because & will operate both condition
+         * 
+         * but 
+         * if(duck!=null&& duck.getAge()<5) if this would done then the null expection will not occer
+         * 
+         * because this is short circuit it will stop at first condition only.
+         * 
+         *  importance of shortcircuit
+         * 
+         * int rb=6;
+         * 
+         * boolean bu=(rb>=6)||(++rb <=7);
+         * 
+         * here only first condition will run 
+         * but we want 
+         * both should be run this is called as side effect .
+         * so here we use |
+         * 
+         * 
+         * ternaory operator = oneLiner ifElse Statement.
+         * 
+         * we can use modulus operator with Decimal value
+         * 
+         * null == null   give true
+         * 
+         * 
+         * Explicit type casting / narrowing
+         * Processing of converting data from higher data type to ;ower data type.
+         * 
+         * byte ranges from -128 to 127
+         * 
+         * for calculating neg value of byte ===  ((n-127)-1)-128
+         * 
+         * ex     ((140-127)-1)-128  =  -116
+         * 
+         *    29 - 8 - 23   27:18
          * 
          * 
          * 
