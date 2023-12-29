eaab5e2665c87905094744ff0563b6a135ffa181
RBRakhitBhardwaj <rb.rakshit.bhardwaj@gmail.com>
2023-12-29 23:09:15 +0530

    more on operator

---
+++ JAVA NOTES/13_operator/BinaryOPerator.java
@@ -0,0 +1,62 @@
+public class BinaryOPerator {
+    public static void main(String[] args) {
+       
+        
+
+
+
+
+
+
+        /*
+         * 
+         * type of variable: {base of datatype}
+         * value type and referenfe type
+         * 
+         * or on the base of location :
+         * local instance or calss variable
+         * 
+         *  
+         * 
+         * 
+         * DEVISOPN OPERATPR:
+         * 
+         * DEvision operator always occupy Quotient[BahgFal] where as
+         * Modulas operator always occupy reminder
+         * 
+         * 23/2
+         * 21%2
+         * 
+         * 
+         * 
+         * Priority of operator:
+         * 
+         * go left to right
+         * First priority: (/,*,%)
+         * Second Priority : (+,-)
+         * 
+         * 
+         * When we have multiple operator of same priority 
+         * 
+         * for arithmatic operation ==> Left to Right
+         * 
+         * for Assignment operator --> Right to left
+         * 
+         * 
+         * 
+         * Compound operator : arithmatic + Assignment:
+         * 
+         * Arithmatic assignment operator:
+         *        Short hand operator:
+         * 
+         *        +=   -=     *=    /=
+         * 
+         * x=x+y   ===   x+=y     both same
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+}
--- JAVA NOTES/13_operator/incrementDecrement2.java
+++ JAVA NOTES/13_operator/incrementDecrement2.java
@@ -3,14 +3,16 @@ public class incrementDecrement2 {
         public static void main(String[] args) {
             int a =10;
             System.out.println(a);     //10
-            // System.out.println(++a);   //11
-            // System.out.println(a++);   //11   --->  12
-            // System.out.println(--a);   //11
-            // System.out.println(a--);   //11   --->  10
-            // System.out.println(a);     //10
+            System.out.println(++a);   //11
+            System.out.println(a++);   //11   --->  12
+            System.out.println(--a);   //11
+            System.out.println(a--);   //11   --->  10
+            System.out.println(a);     //10
         }
         
-    
+             //   6+5-5-6
+             //  (a--+--a)+(a++-++a)*(--a+--a)-(++a+--a);
+             //a=6  (6+4)+(4-6)*(5+4)-(5+4)   
     
     
 }
