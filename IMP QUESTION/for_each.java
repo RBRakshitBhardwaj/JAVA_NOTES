89d8e5946464d3d6a4dfb59e024eb99e2034b6e5
RBRakshitBhardwaj <rb.rakshit.bhardwaj@gmail.com>
2024-03-05 17:37:37 +0530

    print string using for each

---
+++ IMP QUESTION/for_each.java
@@ -0,0 +1,10 @@
+public class for_each {
+    public static void main(String[] args) {
+        String[] names={"java","dsa using java","j2ee","spring","boot"};
+
+        for(String name : names){
+            System.out.println(name);
+        }
+    }
+    
+}
