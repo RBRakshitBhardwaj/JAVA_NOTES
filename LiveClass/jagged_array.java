5cf4d0b1174508cb061c9869eb86836a8544d1c7
RBRakshitBhardwaj <rb.rakshit.bhardwaj@gmail.com>
2024-03-10 00:59:09 +0530

    practice question in calss

---
+++ LiveClass/jagged_array.java
@@ -0,0 +1,12 @@
+package JAVA_NOTES.LiveClass;
+
+public class jagged_array {
+    public static void main(String[] args) {
+        int [][] arr={{1,2,3},{4,5,67,8},{9,10}};
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+        System.out.println(arr[1].length);
+        System.out.println(arr[2].length);
+    }
+    
+}
